2c302c0dfa3a6d707e3b989f052420b0b9be04c6
Indraraj Yadav <indra26@Indrarajs-Mac-mini.local>
2022-12-02 13:24:18 +0530

    feat: add ngModule and standalone component

---
+++ src/app/add-todo/add-todo.component.ts
@@ -0,0 +1,13 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-add-todo',
+  templateUrl: './add-todo.component.html',
+  styleUrls: ['./add-todo.component.scss'],
+  standalone: true,
+})
+export class AddTodoComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit(): void {}
+}
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,13 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { AddTodoComponent } from './add-todo/add-todo.component';
+import { TodoListModule } from './todo-list/todo-list.module';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, TodoListModule, AddTodoComponent],
   styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
